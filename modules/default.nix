7a55af4861514a73a365d920048af9bd1a93b794
Joel Lubinitsky <joellubi@gmail.com>
2024-07-05 17:10:38 -0400

    factor mac home manager into modules

---
+++ modules/default.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  imports =
+    [
+      ./dev.nix
+    ];
+
+  # Home-manager 22.11 requires this be set. We never set it so we have
+  # to use the old state version.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
