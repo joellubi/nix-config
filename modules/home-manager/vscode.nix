5f45f44db76817b455583039917600509356c820
Joel Lubinitsky <joellubi@gmail.com>
2025-07-04 15:59:28 -0400

    migrate vscode config to use profiles

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -18,48 +18,52 @@ in {
     programs.vscode = {
       enable = true;
       package = if config.overrides.vscode.unfree then pkgs.vscode else pkgs.vscodium;
-      userSettings = {
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nixd";
-        "diffEditor.ignoreTrimWhitespace" = false;
+      profiles = {
+        default = {
+          userSettings = {
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "nixd";
+            "diffEditor.ignoreTrimWhitespace" = false;
+          };
+          extensions = with pkgs.vscode-extensions; [
+            eamodio.gitlens
+            golang.go
+            jnoortheen.nix-ide
+            ms-python.python
+            ms-pyright.pyright
+            mkhl.direnv
+            ms-vscode.cpptools-extension-pack
+          ];
+          keybindings = (
+            lib.optionals isLinux [
+              {
+                key = "shift+alt+up";
+                command = "editor.action.copyLinesUpAction";
+                when = "editorTextFocus && !editorReadonly";
+              }
+              {
+                key = "shift+alt+down";
+                command = "editor.action.copyLinesDownAction";
+                when = "editorTextFocus && !editorReadonly";
+              }
+              {
+                key = "ctrl+c";
+                command = "workbench.action.terminal.copySelection";
+                when = "terminalTextSelectedInFocused || terminalFocus && terminalHasBeenCreated && terminalTextSelected || terminalFocus && terminalProcessSupported && terminalTextSelected || terminalFocus && terminalTextSelected && terminalTextSelectedInFocused || terminalHasBeenCreated && terminalTextSelected && terminalTextSelectedInFocused || terminalProcessSupported && terminalTextSelected && terminalTextSelectedInFocused";
+              }
+              {
+                key = "ctrl+v";
+                command = "workbench.action.terminal.paste";
+                when = "terminalFocus && terminalHasBeenCreated || terminalFocus && terminalProcessSupported";
+              }
+              {
+                key = "ctrl+backspace";
+                command = "deleteAllLeft";
+              }
+            ]
+          );
+        };
       };
-      extensions = with pkgs.vscode-extensions; [
-        eamodio.gitlens
-        golang.go
-        jnoortheen.nix-ide
-        ms-python.python
-        ms-pyright.pyright
-        mkhl.direnv
-        ms-vscode.cpptools-extension-pack
-      ];
-      keybindings = (
-        lib.optionals isLinux [
-          {
-            key = "shift+alt+up";
-            command = "editor.action.copyLinesUpAction";
-            when = "editorTextFocus && !editorReadonly";
-          }
-          {
-            key = "shift+alt+down";
-            command = "editor.action.copyLinesDownAction";
-            when = "editorTextFocus && !editorReadonly";
-          }
-          {
-            key = "ctrl+c";
-            command = "workbench.action.terminal.copySelection";
-            when = "terminalTextSelectedInFocused || terminalFocus && terminalHasBeenCreated && terminalTextSelected || terminalFocus && terminalProcessSupported && terminalTextSelected || terminalFocus && terminalTextSelected && terminalTextSelectedInFocused || terminalHasBeenCreated && terminalTextSelected && terminalTextSelectedInFocused || terminalProcessSupported && terminalTextSelected && terminalTextSelectedInFocused";
-          }
-          {
-            key = "ctrl+v";
-            command = "workbench.action.terminal.paste";
-            when = "terminalFocus && terminalHasBeenCreated || terminalFocus && terminalProcessSupported";
-          }
-          {
-            key = "ctrl+backspace";
-            command = "deleteAllLeft";
-          }
-        ]
-      );
     };
 
   };
