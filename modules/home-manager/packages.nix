8792b838994bed37950760abf10f38d7f6f0bb2c
Joel Lubinitsky <joellubi@gmail.com>
2025-07-04 15:59:14 -0400

    restructure packages and add kube alias

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -12,33 +12,39 @@ in {
     # Packages
     #---------------------------------------------------------------------
 
-    home.packages = [
-      pkgs.curl
-      pkgs.delve
-      pkgs.docker-compose
-      pkgs.duckdb
-      pkgs.go
-      pkgs.google-cloud-sdk
-      pkgs.gotools
-      pkgs.graphviz
-      pkgs.jq
-      pkgs.just
-      pkgs.kubectl
-      pkgs.lf
-      pkgs.nixd
-      pkgs.postgresql_16
-      pkgs.redis
-      pkgs.rustup
-      pkgs.terraform
-      pkgs.tree
-      pkgs.vim
-      pkgs.wget
-      pkgs.wireguard-tools
-      pkgs.yarn
-    ] ++ (lib.optionals isLinux [
-      pkgs.xclip
-      pkgs.spotify
-    ]);
+    home = {
+      packages = [
+        pkgs.curl
+        pkgs.delve
+        pkgs.docker-compose
+        pkgs.duckdb
+        pkgs.go
+        pkgs.google-cloud-sdk
+        pkgs.gotools
+        pkgs.graphviz
+        pkgs.jq
+        pkgs.just
+        pkgs.kubectl
+        pkgs.lf
+        pkgs.nixd
+        pkgs.nmap
+        pkgs.postgresql_16
+        pkgs.redis
+        pkgs.rustup
+        pkgs.terraform
+        pkgs.tree
+        pkgs.vim
+        pkgs.wget
+        pkgs.wireguard-tools
+        pkgs.yarn
+      ] ++ (lib.optionals isLinux [
+        pkgs.xclip
+        pkgs.spotify
+      ]);
+      shellAliases = {
+        k = "kubectl";
+      };
+    };
 
   };
 
